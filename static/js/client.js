c2011a368f347183677f65382fb641998ede6379
harsh <109368583+harshsharma1981@users.noreply.github.com>
2022-08-09 11:29:04 +0530

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -81,7 +81,7 @@ socket.on("type-receive", (n2) => {
   if ((typ.style.display = "block")) {
     setTimeout(function () {
       typ.style.display = "none";
-    }, 10000);
+    }, 2000);
   }
   const scrollToBottom = (node) => {
     node.scrollTop = node.scrollHeight;
